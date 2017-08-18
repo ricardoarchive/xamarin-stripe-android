2a241ea6456d85aded03ecd542faf11bf93b644d
Adrian Hoitan <adrian.hoitan@ricardo.ch>
2017-08-18 14:29:20 +0200

    Initial : add Stripe binding + sample

---
+++ Ricardo.Stripe.Android/Ricardo.Stripe.Android.Sample/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Com.Stripe.Android;
+using Com.Stripe.Android.Model;
+using Com.Stripe.Android.View;
+
+namespace Ricardo.Stripe.Android.Sample
+{
+    [Activity(Label = "Ricardo.Stripe.Android.Sample", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
